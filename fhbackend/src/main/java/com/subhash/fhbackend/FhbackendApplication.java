f2e69590e53cf49be3433d5550108b2980d890fc
subhash18cH <subhashcnd15@gmail.com>
2025-03-11 18:40:03 +0530

    project setup

---
+++ fhbackend/src/main/java/com/subhash/fhbackend/FhbackendApplication.java
@@ -0,0 +1,13 @@
+package com.subhash.fhbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FhbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FhbackendApplication.class, args);
+	}
+
+}
---
+++ fhbackend/src/test/java/com/subhash/fhbackend/FhbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.subhash.fhbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FhbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
